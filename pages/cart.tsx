a08137da293a17ad4cd3cb3d04c68677c7a96e1c
Kamrul Hasan <kamrul@kamruls-mbp-2.mynetworksettings.com>
2024-09-23 13:15:52 -0400

    Fixed issue with cart page component

---
+++ pages/cart.tsx
@@ -0,0 +1,14 @@
+// pages/cart.js
+
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div>
+      <h1>Your Shopping Cart</h1>
+      {/* Other content for the cart */}
+    </div>
+  );
+};
+
+export default Cart; // Ensure you export the component as default
